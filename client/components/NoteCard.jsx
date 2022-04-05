c4a7b23600c233a2df94376d92a368d897587c74
Leonoor Rinke de Wit <lrinkedewit@gmail.com>
2022-04-05 12:31:34 -0400

    changes to NoteCard

---
+++ client/components/NoteCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function NoteCard(props) {
+
+
+    return (
+
+
+
+        <div class="card">
+            <div class="card__side card__side--front">
+                <div class="card__picture card__picture--1">
+                    &nbsp;
+                </div>
+                <h4 class="card__heading">
+                    <span class="card__heading-span card__heading-span--1">{props.name}</span>
+                </h4>
+                <div class="card__details">
+                    <p>
+                        {props.description}
+                    </p>
+                </div>
+            </div>
+            <div class="card__side card__side--back card__side--back-1">
+                <div class="card__cta">
+                    <div class="card__price-box">
+                        <p class="card__price-only">
+                            Only
+                        </p>
+                        <p class="card__price-value">{'$' + props.price}</p>
+                    </div>
+                    <button>Add to Cart!</button>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default NoteCard;
--- client/features/store.js
+++ client/features/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './features/slices/counterSlice';
+import counterReducer from './slices/counterSlice';
 
 export const store = configureStore({
     reducer: {
